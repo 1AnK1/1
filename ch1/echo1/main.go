e67a00360c90889fcf900df5ed1c6bc9b2eda49a
1AnK1 <angelikakostarevais1249@gmail.com>
2026-09-18 11:42:10 +0500

    Add files via upload

---
+++ ch1/echo1/main.go
@@ -0,0 +1,15 @@
+gopl.io/chl/echol
+//Echol выводит аргументы командной строки
+package main
+import (
+ "fmt"
+ "os"
+)
+func main() {
+var s, sep string
+for i := 1; i < len(os.Args); i++ {
+s += sep + os.Args [i]
+sep = " "
+}
+fmt.Println(s)
+}
